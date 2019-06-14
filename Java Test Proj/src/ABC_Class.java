287ab30eb88ef876ecdf131404cc33f92c740554
Gaurankv <Gaurankv@LAPTOP17.in.kovair.com>
2019-06-14 18:11:00 +0530

    ABC_Class class added

---
+++ Java Test Proj/src/ABC_Class.java
@@ -0,0 +1,13 @@
+
+public class ABC_Class {
+
+	public ABC_Class() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ABCCCCCCCCCCCCCCCCCc");
+	}
+
+}
