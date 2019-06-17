24d801f4b422f0c76cec8c0b3fe1b0e39b98e383
Gaurankv <Gaurankv@LAPTOP17.in.kovair.com>
2019-06-17 17:23:03 +0530

    HMTL

---
+++ Java Test Proj/src/HTML_Template.java
@@ -0,0 +1,17 @@
+
+
+public class HTML_Template {
+
+	public HTML_Template() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i  = 0;
+		if( i == 0) {
+			System.out.println("HTML_Template");
+		}
+	}
+
+}
