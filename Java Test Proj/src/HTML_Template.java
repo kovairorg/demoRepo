e683c83d8e6e200de44138504a70208def5e08bc
Gaurankv <Gaurankv@LAPTOP17.in.kovair.com>
2019-06-17 18:33:57 +0530

    RadioCkb

--- Java Test Proj/src/HTML_Template.java
+++ Java Test Proj/src/HTML_Template.java
@@ -17,6 +17,7 @@ public class HTML_Template {
 			System.out.println("HTML_Template : Textbox, Button value Special Characters");
 			System.out.println("HTML_Template : Textbox, Blank");
 			System.out.println("HTML_Template : Textbox, Special Chars");
+			System.out.println("HTML_Template : Radio & Checkbox");
 		}
 	}
 
