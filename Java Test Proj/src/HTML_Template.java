f6cb65b77112100cc531ff84e49b9f03ad553d48
Gaurankv <Gaurankv@LAPTOP17.in.kovair.com>
2019-06-17 18:13:05 +0530

    BlankValue

--- Java Test Proj/src/HTML_Template.java
+++ Java Test Proj/src/HTML_Template.java
@@ -15,7 +15,7 @@ public class HTML_Template {
 			System.out.println("HTML_Template : Underline, select, UL & OL");
 			System.out.println("HTML_Template : Textbox, Button");
 			System.out.println("HTML_Template : Textbox, Button value Special Characters");
-
+			System.out.println("HTML_Template : Textbox, Blank");
 		}
 	}
 
