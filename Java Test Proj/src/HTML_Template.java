f43514879ae0a2f353f79083b89593a9ec7fc412
Gaurankv <Gaurankv@LAPTOP17.in.kovair.com>
2019-06-17 18:45:11 +0530

    Color change

--- Java Test Proj/src/HTML_Template.java
+++ Java Test Proj/src/HTML_Template.java
@@ -18,7 +18,7 @@ public class HTML_Template {
 			System.out.println("HTML_Template : Textbox, Blank");
 			System.out.println("HTML_Template : Textbox, Special Chars");
 			System.out.println("HTML_Template : Radio & Checkbox");
-			System.out.println("HTML_Template : Blank Commit Msg");
+			System.out.println("HTML_Template : Blank Commit Msg ");
 		}
 	}
 
